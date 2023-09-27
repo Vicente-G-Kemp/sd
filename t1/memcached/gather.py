bcb7908f076e2a9a0df92619be2c4f628d9d39d7
Vicente-G-Kemp <vichogk@gmail.com>
2023-09-26 23:24:12 -0300

    QoL

--- t1/memcached/gather.py
+++ t1/memcached/gather.py
@@ -8,13 +8,19 @@ def run_gather(access_counter, lock, min):
     while True:
         if access_counter.keys():
             break
+    popper = list()
     while True:
         with lock:
             for key, count in access_counter.items():
                 if count < min:
                     ap.delete("id_"+key)
-                    print("removed item: id_"+key)
+                    # print("removed item: id_"+key)
+                    popper.append(key) #access_counter.pop(key)
                 else:
                     print("Not removed: id_"+key)
-            access_counter[key] = 0
+                    access_counter[key] = 0
+
+        for p in popper:
+            print("removed item: id_"+p)
+            access_counter.pop(p, None)
         time.sleep(8)
--- t1/memcached/mem_client.py
+++ t1/memcached/mem_client.py
@@ -15,7 +15,7 @@ min_access = 2
 
 thread = threading.Thread(target=run_gather, args=(access_counter, lock, min_access))
 thread.daemon = True
-thread.start()
+# thread.start()
 
 def update_counter(key):
     with lock:
@@ -25,8 +25,7 @@ def update_counter(key):
             access_counter[key]=1
 
 ap = memcache.Client(['172.17.0.2:11211'])
-
-def Get(key):
+def Get(key, simulated=False):
     start_time = time.time()
 
     delay = np.random.normal(2, 0.5)
@@ -38,7 +37,10 @@ def Get(key):
         elapsed_time = time.time() - start_time
         print(f"Time taken: {elapsed_time:.5f} seconds")
         update_counter(key)
-        return response
+        if not simulated:
+            return response
+        else:    
+            return response, delay-elapsed_time
     else:
         print(f"Key not found in cache. Waiting {delay:.5f} seconds...")
         time.sleep(delay)
@@ -50,24 +52,73 @@ def Get(key):
             elapsed_time = time.time() - start_time
             print(f"Time taken: {elapsed_time:.5f} seconds")
             update_counter(key)
-            return value
+            return value, 0
         else:
             print("Key not found.")
             return None
+
+def sim(n_searches, sim_type):
+    if sim_type:
+        keys_to_search = [f"{i}" for i in np.random.randint(0, 100, n_searches)]
+    else:
+        keys_to_search = list()
+        i=0
+        while i<n_searches:
+            hold_id = int(random.normalvariate(50,20))
+            hold_id = max(0, min(99, hold_id))
+            keys_to_search.append(f"{hold_id}")
+            i+=1
+
+    total_time = 0
+    avoided_json_lookups = 0
+    expected_delay = 0
+    count = 0
+
+    for key in keys_to_search:
+        # clear console
+        count += 1
+        print("\033[H\033[J")
+        print(f"Searching : {count}/{n_searches}")
+        start_time = time.time()
+
+        value, delay = Get(key, True)
+
+        elapsed_time = time.time() - start_time
+
+        total_time += elapsed_time
+
+        if elapsed_time < 0.5:
+            avoided_json_lookups += 1
+            expected_delay += delay
             
+
+    time_saved = expected_delay
+
+    print(f"\nTime saved thanks to cache: {time_saved:.2f} seconds")
+    print(f"Number of times JSON lookup was avoided: {avoided_json_lookups}")
+    print(f"Total search time: {total_time}")
+    return None
     
 while True:
     print("\nSelect an action:")
     print("1. Get")
+    print("2. simulate")
+    print("7. Eviction mode")
     print("9. exit")
     choice = input("Standing by... ")
 
     if choice == "1":
         key = input("Enter key: ")
         print(Get(key))
-    if choice == "8":
+    elif choice == "2":
+        n_searches = int(input("Enter the number of searches you want to simulate: "))
+        sim_type = bool(int(input("Choose the type of simulation: 1 - regular, 0 - normal/gaussian\n")))
+        sim(n_searches, sim_type)
+    elif choice == "7":
+        thread.start()
+    elif choice == "8":
         setup()
-    if choice == "9":
+    elif choice == "9":
         break
     else:
         print("opción inválida")
